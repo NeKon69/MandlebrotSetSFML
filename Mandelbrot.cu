449bac40389bc7a507f0f74ab0dc4d8fb890c621
NeKon69 <nobodqwe@gmail.com>
2025-03-15 21:26:02 +0300

    added comments for you bro to read

--- Mandelbrot.cu
+++ Mandelbrot.cu
@@ -9,8 +9,11 @@
 #include <thread>
 
 
-
-unsigned int max_iterations = 500;
+/**
+ * @brief Maximum number of iterations for Mandelbrot set calculation.
+ * Higher values increase detail but also computation time.
+ */
+unsigned int max_iterations = 2000;
 
 double basic_zoom_x = 240.0f;
 double basic_zoom_y = 240.0f;
@@ -30,7 +33,14 @@ bool is_dragging = false;
 unsigned int width = 1600;
 unsigned int height = 1200;
 
+/**
+ * @brief Pixel buffer in host memory to store the rendered image data.
+ */
 unsigned char* pixels = new unsigned char[width * height * 4];
+
+/**
+ * @brief Pixel buffer in device (GPU) memory, used for CUDA kernel.
+ */
 unsigned char* d_pixels;
 
 #ifndef M_PI
@@ -93,6 +103,13 @@ void HSVtoRGB(double h, double s, double v, unsigned int& r, unsigned int& g, un
     b = static_cast<unsigned int>((b_ + m) * 255);
 }
 
+
+/**
+ * @brief Creates a color palette using HSV color space and converts it to RGB.
+ * The palette is designed to smoothly transition colors for visualization of the Mandelbrot set.
+ * @param numColors The number of colors in the palette.
+ * @return std::vector<sf::Color> A vector containing the generated color palette.
+ */
 std::vector<sf::Color> createHSVPalette(int numColors) {
     std::vector<sf::Color> palette;
     for (int i = 0; i < numColors; ++i) {
@@ -119,12 +136,27 @@ std::vector<sf::Color> createHSVPalette(int numColors) {
 }
 
 std::vector<sf::Color> palette = createHSVPalette(200000);
-
 sf::Image image({ 1600, 1200 }, sf::Color::Black);
 
 
-__global__
-void mandelbrot(unsigned char* pixels, int width, int height, double zoom_x, double zoom_y, double x_offset, double y_offset, sf::Color* d_palette, int paletteSize, double maxIterations) {
+/**
+ * @brief CUDA kernel function to calculate and render the Mandelbrot set.
+ * This kernel is executed in parallel by multiple threads on the GPU.
+ * Each thread calculates the color of a single pixel based on its position
+ * and the Mandelbrot set algorithm.
+ *
+ * @param pixels Pointer to the pixel data buffer in device memory.
+ * @param width Image width.
+ * @param height Image height.
+ * @param zoom_x Zoom level along the x-axis.
+ * @param zoom_y Zoom level along the y-axis.
+ * @param x_offset Offset in the x-direction to move the view.
+ * @param y_offset Offset in the y-direction to move the view.
+ * @param d_palette Color palette in device memory to color the Mandelbrot set.
+ * @param paletteSize Size of the color palette.
+ * @param maxIterations Maximum iterations for Mandelbrot calculation.
+ */
+__global__ void mandelbrot(unsigned char* pixels, int width, int height, double zoom_x, double zoom_y, double x_offset, double y_offset, sf::Color* d_palette, int paletteSize, double maxIterations) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -152,9 +184,16 @@ void mandelbrot(unsigned char* pixels, int width, int height, double zoom_x, dou
             r = g = b = 0;
         }
         else {
+            // Color gradient calculation based on iteration count for points outside the set
             double max_iters = maxIterations;
+
+            // Smooth iteration count
             current_iteration = current_iteration + 1 - dev_log2(dev_log2(dev_abs(dev_sqrt(complex_abs2(z_real, z_imag)))));
+
+            // Calculate gradient value
             double gradient = Gradient(current_iteration, max_iters);
+
+            // Map gradient to palette index
             int index = static_cast<int>(gradient * (paletteSize - 1));
             sf::Color color = getPaletteColor(index, paletteSize, d_palette);
             r = color.r;
@@ -169,6 +208,19 @@ void mandelbrot(unsigned char* pixels, int width, int height, double zoom_x, dou
     }
 }
 
+
+
+/**
+ * @brief Applies 4x Super-Sampling Anti-Aliasing (SSAA) to reduce aliasing artifacts.
+ * This function takes a high-resolution image (2x width and height of the desired output)
+ * and downsamples it to produce a smoother, anti-aliased image. It averages the color
+ * of 2x2 pixel blocks in the high-resolution image to generate each pixel in the
+ * low-resolution image.
+ *
+ * @param high_qual The high-resolution input image.
+ * @param size The scaling factor (should be 2 for 4x SSAA).
+ * @return sf::Image The anti-aliased, low-resolution image.
+ */
 static sf::Image ANTIALIASING_SSAA4(sf::Image& high_qual, unsigned int size) {
     sf::Image low_qual({ high_qual.getSize().x / size * 2, high_qual.getSize().y / size * 2 }, sf::Color::Black);
     for (unsigned int x = 0; x < high_qual.getSize().x; x += 2) {
@@ -184,6 +236,14 @@ static sf::Image ANTIALIASING_SSAA4(sf::Image& high_qual, unsigned int size) {
     return low_qual;
 }
 
+
+/**
+ * @brief Handles zoom functionality based on mouse wheel input.
+ * Adjusts the zoom factor and offsets to zoom in or out centered around the mouse position.
+ *
+ * @param wheel_delta The direction and magnitude of the mouse wheel scroll.
+ * @param mouse_pos The current mouse position in window coordinates.
+ */
 void handleZoom(float wheel_delta, const sf::Vector2i mouse_pos) {
     double old_zoom_x = zoom_x;
     double old_zoom_y = zoom_y;
@@ -191,13 +251,14 @@ void handleZoom(float wheel_delta, const sf::Vector2i mouse_pos) {
     double old_y_offset = y_offset;
 
     double zoom_change = 1.0 + wheel_delta * zoom_speed;
-    zoom_factor *= zoom_change;
+    zoom_factor *= zoom_change; // Update zoom factor based on wheel delta
 
     zoom_factor = std::max(std::min(zoom_factor, 1000000000000.0), 0.01);
 
     zoom_x = basic_zoom_x * zoom_factor;
     zoom_y = basic_zoom_y * zoom_factor;
 
+    // Calculate the change in offset to keep the zoom centered at the mouse position
     double image_mouse_x = mouse_pos.x * (1600.0 / 800.0);
     double image_mouse_y = mouse_pos.y * (1200.0 / 600.0);
 
@@ -207,11 +268,25 @@ void handleZoom(float wheel_delta, const sf::Vector2i mouse_pos) {
     std::cout << "Zoom Factor: " << zoom_factor << ", Offset: (" << x_offset << ", " << y_offset << ")" << std::endl;
 }
 
+
+/**
+ * @brief Starts the dragging operation when the mouse button is pressed.
+ * Records the starting mouse position.
+ *
+ * @param mouse_pos The current mouse position.
+ */
 void start_dragging(sf::Vector2i mouse_pos) {
     is_dragging = true;
     drag_start_pos = mouse_pos;
 }
 
+
+/**
+ * @brief Handles the dragging operation while the mouse is moved and the button is held down.
+ * Updates the view offset based on the mouse movement since the drag started.
+ *
+ * @param mouse_pos The current mouse position.
+ */
 void dragging(sf::Vector2i mouse_pos) {
     if (!is_dragging) return;
 
@@ -226,11 +301,20 @@ void dragging(sf::Vector2i mouse_pos) {
     drag_start_pos = mouse_pos;
 }
 
+
+
+/**
+ * @brief Stops the dragging operation when the mouse button is released.
+ * Resets the dragging flag.
+ */
 void stop_dragging() {
     is_dragging = false;
 }
 
+
+
 int main() {
+	// Used to render or not the image based on the moves made with the mouse
     bool need_render = true;
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Mandelbrot");
     sf::Image image({ 1600, 1200 }, sf::Color::Black);
@@ -241,13 +325,13 @@ int main() {
 
     sf::Vector2i mouse;
 
-    // Palette transfer to device
+    // Palette transfer to device memory for CUDA kernel access
     sf::Color* d_palette;
     int paletteSize = palette.size();
-    cudaMalloc(&d_palette, paletteSize * sizeof(sf::Color));
-    cudaMemcpy(d_palette, palette.data(), paletteSize * sizeof(sf::Color), cudaMemcpyHostToDevice);
+    cudaMalloc(&d_palette, paletteSize * sizeof(sf::Color)); // Allocate device memory for palette
+    cudaMemcpy(d_palette, palette.data(), paletteSize * sizeof(sf::Color), cudaMemcpyHostToDevice); // Copy palette data to device
 
-    cudaMalloc(&d_pixels, width * height * 4 * sizeof(unsigned char));
+    cudaMalloc(&d_pixels, width * height * 4 * sizeof(unsigned char)); // Allocate device memory for pixels
 
     while (window.isOpen()) {
         while (const auto event = window.pollEvent()) {
@@ -287,16 +371,24 @@ int main() {
         }
 
         if (need_render) {
-            dim3 dimBlock(128, 128);
+            // Configure CUDA kernel execution grid and block dimensions
+            dim3 dimBlock(256, 256);
             dim3 dimGrid((width + dimBlock.x - 1) / dimBlock.x, (height + dimBlock.y - 1) / dimBlock.y);
+
+            // Launch Mandelbrot CUDA kernel
             mandelbrot <<<dimBlock, dimGrid>>> (d_pixels, width, height, zoom_x, zoom_y, x_offset, y_offset, d_palette, paletteSize, max_iterations);
             cudaDeviceSynchronize();
+
+
+            // Copy rendered pixels back to host
             cudaMemcpy(pixels, d_pixels, width * height * 4 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
 
             sf::Image image1({ width, height }, pixels);
             image = image1;
             need_render = false;
 
+
+            // Apply 4x SSAA
             compressed = ANTIALIASING_SSAA4(image, 4);
 
             window.clear();
@@ -311,10 +403,13 @@ int main() {
             auto time = timer.restart();
             std::cout << "Mandelbrot set was drew in: " << time.asMilliseconds() << std::endl;
 
+
+            // Display the rendered frame
             window.display();
         }
     }
 
+    // Free device memory
     cudaFree(d_pixels);
     cudaFree(d_palette);
 
